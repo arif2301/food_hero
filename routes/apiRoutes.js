aed09338a57e270bca5e3b7318d3c7bcd51f6086
VonKruegs <rob.krueger@gmail.com>
2019-09-14 14:55:08 -0400

    APIRoutes for Login and Register

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,51 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // 
+  app.get("/api/example", function(req, res) {
+    db.foodherodb.findAll({}).then(function(dbfoodherodb) {
+      res.json(foodherodb);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new registration for each :type
+  app.post("/api/register/:type", function(req, res) {
+    if (req.params.type ==="hero") {
+      db.registration.create({name: req.body.name, email:req.body.email, password: req.body.password, type: req.params.type}).then(function(user) {
+        res.json(user);
+      });
+    }
+    else if (req.params.type === "donor") {
+      db.registration.create({email:req.body.email, password: req.body.password, type: req.params.type, streetnumber: req.body.streetnumber, streetname: req.body.streetname, suite: req.body.suite, city: req.body.city, postal: req.body.postal, province: req.body.province, country: req.body.country}).then(function(user) {
+        res.json(user);
+      })
+    }
+    else {
+      db.registration.create({email:req.body.email, password: req.body.password, type: req.params.type, streetnumber: req.body.streetnumber, streetname: req.body.streetname, suite: req.body.suite, city: req.body.city, postal: req.body.postal, province: req.body.province, country: req.body.country}).then(function(user) {
+        res.json(user);
+      })
+    }
+
+
+
+  //Do a login check
+
+  app.post("/api/login", function(req, res) {
+    db.registration_donor.findOne({where: {email: req.body.email}}).then(function(user) {
+    if (req.body.password === user.password){
+      return res.json(user);
+    }
+    res.send("Please try again!");
+
+      
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
-};
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.json(dbExample);
+  //   });
+  }
