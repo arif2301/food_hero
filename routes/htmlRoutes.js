1cd02a9e103d90de2ee02f1cf3a3b44c82754feb
neil <arifrahman@hotmail.com>
2019-09-16 21:26:57 -0400

    html routes started

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,57 @@
+// html-routes.js - for Food Hero
+
+var path = require("path");
+
+module.exports = function(app) {
+  //home page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+
+  
+  //main page - for log 
+  //registration page for all
+  app.get("/registration", function(req, res) {
+    res.sendFile(path.join(__dirname, "../registration.html"));
+  });
+  
+  //page to enter donation - for donor
+  app.get("/donor", function(req, res) {
+    res.sendFile(path.join(__dirname, "../donor.html"));
+  });
+  
+  //recipient page, after they logged in or registered - let them know they are on the list for recieving donations
+  app.get("/recipient", function(req, res) {
+    res.sendFile(path.join(__dirname, "../recipient.html"));
+  });
+  
+  //mission offer page - accept or decline - for food hero
+  app.get("/foodhero/offer", function(req, res) {
+    res.sendFile(path.join(__dirname, "../foodhero.html"));
+  });
+
+  //mission page, it will show route and have buttons to click when pickup and the dropoff-food hero 
+  app.get("/foodhero/mission", function(req, res) {
+    res.sendFile(path.join(__dirname, "../mission .html"));
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
 var db = require("../models");
 
 module.exports = function(app) {
