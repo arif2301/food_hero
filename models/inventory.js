808bf8193844d62ac9ceecb4f1c119e43b4a6f75
neil <arifrahman@hotmail.com>
2019-09-17 19:34:25 -0400

    redoing models

--- models/inventory.js
+++ models/inventory.js
@@ -3,7 +3,7 @@
 // Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to the DB.
-//var sequelize = require("../config/connection.js");
+var sequelize = require("../config/connection.js");
 // Creates a "Inventory" model that matches up with DB
 module.exports = function(sequelize, DataTypes) {
   var Inventory = sequelize.define(
